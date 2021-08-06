f107cf9ea5f80f9b03ee102bcc8e5aeb3bb9d048
Kiruthika.k <Kiruthika.k@host.docker.internal>
2021-08-06 15:08:41 +0530

    pagination added

--- ManageCabs/src/main/resources/static/js/managecabsfinal.js
+++ ManageCabs/src/main/resources/static/js/managecabsfinal.js
@@ -1,4 +1,7 @@
+window.sessionStorage;
 
+
+//var countPerPage = sessionStorage.getItem("countPerPage");
 
         var xhrCabModel = new XMLHttpRequest();
         var xhrDriverInfo = new XMLHttpRequest();
@@ -201,7 +204,9 @@ document.addEventListener("click", function(e) {
              var arr = JSON.parse(xhrCabDetails.responseText);
            
              rowCounter = 0;
-       
+             
+       		 sessionStorage.setItem("cabCount",arr.length);
+       		 
        // Iterate the cabDetails       
        for(var i=0;i<arr.length; i++){                              
 
@@ -277,13 +282,11 @@ document.addEventListener("click", function(e) {
         document.getElementById("cab-info").appendChild(trow);
   
   	}
-  	    //To display the cab count
-  	var count = document.createElement('div');
-    count.class = "header-left py-md-3";
-    document.getElementById("displayCount").innerText ='Display : '+ rowCounter+" cabs out of " +rowCounter;
+  	
+    cabPager.showCabPage(1);
         }
         
-
+ 	
   } 
   
  /* ------------------------------------------------------------------------------------------------------------------------*/
@@ -426,7 +429,7 @@ l	    }
   {	
 	if(getDriverId==undefined)
 	{
-		alert("select from given list");
+		alert("select driver from given list");
 		return false;
 	}
 	var data = {"cabModel":cabModel,"cabNumber":cabNumber,"totalSeats":totalSeats,
@@ -477,6 +480,7 @@ l	    }
      var response = this.responseText;
      alert("Cab Details saved successfully");
      getDriverId = undefined;
+     //location.reload();
      funclear();
    }
    
@@ -590,7 +594,7 @@ function funclear()
                 document.getElementById("insurance-num").value="";
                 document.getElementById("ins-exp-date").value=true;
                 document.getElementById("driv-name").value="";
-                               
+                              
             }
 
 function Reset() {
@@ -788,3 +792,189 @@ function enterEventDriverName(event){
     }   
 }
 
+
+
+/*----------------------------------------------------------------------------------------------------------------------------- */
+// pagination starts here
+window.sessionStorage;
+   
+    //Display value
+    var currentPage=1;
+    var totPages=1;
+    var records;
+    var PerPagesize = 10;
+    
+    //sessionStorage.setItem("countPerPage",PerPagesize);
+    
+    var pagerName;
+    var cabCount = sessionStorage.getItem("cabCount");
+    
+    var cabPager = new cabPager('cab-info', PerPagesize);
+    cabPager.init();
+    cabPager.showPageNav('cabPager', 'cabPageNavPosition');
+
+    cabPager.showCabPage(1);
+    var noOfPages=this.currentPage *PerPagesize;
+    if(noOfPages>records){
+      this.noOfPages = records;
+    }
+    
+    var showingPage = document.getElementById('showCabPage'); 
+    function cabPager(tableName, itemsPerPage) {
+
+      this.tableName = tableName;
+      this.currentPage = 1;
+      this.pages = 0;
+      this.inited = false;
+        
+      this.showRecords = function (from, to) {
+        var rows = document.getElementById(tableName).rows;
+        for (var i = 0; i < rows.length; i++) {
+          if (i < from || i > to)
+            rows[i].style.display = 'none';
+          else
+            rows[i].style.display = '';
+        }
+      }
+
+      this.showCabPage = function (pageNumber) {
+
+        if (!this.inited) {
+          alert("not inited");
+          return;
+        }
+
+        var oldPageAnchor = document.getElementById('pg' + this.currentPage);
+       oldPageAnchor.className = 'pg-normal';
+
+        this.currentPage = pageNumber;
+        var newPageAnchor = document.getElementById('pg' + this.currentPage);
+       newPageAnchor.className = 'pg-selected';
+
+        var from = (pageNumber - 1) * (Number(itemsPerPage));
+        var to = (from + Number(itemsPerPage)) - 1;
+        this.showRecords(from, to);
+		var pagerName = 'cabPager';
+
+        var pgNext = document.getElementById(pagerName + 'pgNext');
+
+        var pgPrev = document.getElementById(pagerName + 'pgPrev');
+
+        if (pgNext != null) {
+          if (this.currentPage == this.pages) pgNext.style.display = 'none';
+          else pgNext.style.display = '';
+        }
+        if (pgPrev != null) {
+          if (this.currentPage == 1) pgPrev.style.display = 'none';
+          else pgPrev.style.display = '';
+        }
+        var noOfPages=this.currentPage * PerPagesize;
+
+        if(noOfPages>records){
+          noOfPages = records;
+        }
+        
+        var showingPage = document.getElementById('showCabPage');
+        
+		//To display the cab count
+		document.getElementById("displayCount").innerText ='Display : '+ $('#cab-info > tr:not([style*="display: none"])').length+" out of " +rowCounter;
+
+
+      }
+
+      this.prev = function () {
+        if (this.currentPage > 1) {
+          this.createPager(this.currentPage - 4, this.currentPage); // it  used to move the cursor to privious index.
+          this.showCabPage(this.currentPage - 1);
+        }
+      }
+
+      this.next = function () {
+        if (this.currentPage < this.pages) {
+          this.createPager(this.currentPage, this.currentPage + 4);  //when i click the next button it is count the records      upto 5 . So indexing   is mainting the standed.
+
+          this.showCabPage(this.currentPage + 1);  //it is used to print  imediate next records.
+
+
+        }
+      }
+
+      this.init = function () {
+        var rows = cabCount;
+       records = (rows);
+        this.pages = Math.ceil(records / itemsPerPage);
+        this.inited = true;
+
+      }
+
+
+
+      //it is used to  move the cursor upto 5 records.
+      this.createPager = function (startEle, EndEle) {
+
+
+        if (EndEle <= this.pages && startEle > 0) {
+
+          var pagerName = document.querySelector(':root');
+          pagerName = 'cabPager';
+
+
+
+
+          var pagerHtml = ' <span id="' + pagerName + 'pgPrev" onclick="' + pagerName + '.prev();" class="pg-normal">  <img src="images/prev-arrow.jpg" alt="page-arrow" width="18" height="15"/> </span> &nbsp; ';
+
+
+          
+         var element = document.getElementById("cabPageNavPosition");
+          for (var page = startEle; page <= EndEle; page++)
+
+
+            pagerHtml += ' <span id="pg' + page + '" class="pg-normal" onclick="' + pagerName + '.showCabPage(' + page + ');">' + page + ' </span> &nbsp; ';;
+
+
+          pagerHtml += ' <span id="' + pagerName + 'pgNext" onclick="' + pagerName + '.next();" class="pg-normal">   <img id ="pagination-arrow" src="image/next-arrow.jpg" width="18" height="15" alt="page-arrow" /> </span> ';
+
+          element.innerHTML = pagerHtml;
+
+        }
+
+      }
+
+      this.showPageNav = function (pagerName, positionId) {
+
+        if (!this.inited) {
+          alert("not inited");
+          return;
+        }
+
+
+        var pagerHtml = ' <span id="' + pagerName + 'pgPrev" onclick="' + pagerName + '.prev();" class="pg-normal"> <img src="Images/pagination-prev-arrow.svg" alt="page-arrow" width="18" height="15"/> </span> &nbsp;  ';
+
+
+        //for printing number of indexes per page
+
+        if (this.pages >= 5) {
+          
+          this.totPages = 5
+        }
+        else {
+          this.totPages = this.pages;
+        }
+
+        //it is used to print the  fixed 5 records.
+        var element = document.getElementById(positionId);
+
+        for (var page = 1; page <= this.totPages; page++)
+        pagerHtml += ' <span id="pg' + page + '" class="pg-normal" onclick="' + pagerName + '.showCabPage(' + page + ');">' + page + '  </span> &nbsp;';
+        if(this.currentPage != this.totPages){
+
+        pagerHtml += ' <span id="' + pagerName + 'pgNext" onclick="' + pagerName + '.next();" class="pg-normal">  <img src="Images/pagination-next-arrow.svg" width="18" height="15" alt="page-arrow" />    </span> ';
+        }
+        element.innerHTML = pagerHtml;
+      }
+
+      
+    }
+
+    
+ /* --------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- */   
---
+++ ManageCabs/src/main/resources/static/js/pagination1.js
@@ -0,0 +1,184 @@
+
+
+
+window.sessionStorage;
+   
+    //Display value
+    var currentPage=1;
+    var totPages=1;
+    var records;
+    var PerPagesize = 3;
+    sessionStorage.setItem("countPerPage",PerPagesize);
+    var pagerName;
+    var driverCount = sessionStorage.getItem("driverCount");
+    
+    var pager = new Pager('cab-info', PerPagesize);
+    pager.init();
+    pager.showPageNav('pager', 'pageNavPosition');
+
+    pager.showPage(1);
+    var noOfPages=this.currentPage *PerPagesize;
+    if(noOfPages>records){
+      this.noOfPages = records;
+    }
+    
+    var showingPage = document.getElementById('showPage'); 
+    function Pager(tableName, itemsPerPage) {
+
+      this.tableName = tableName;
+      this.currentPage = 1;
+      this.pages = 0;
+      this.inited = false;
+        
+      this.showRecords = function (from, to) {
+        var rows = document.getElementById(tableName).rows;
+        for (var i = 0; i < rows.length; i++) {
+          if (i < from || i > to)
+            rows[i].style.display = 'none';
+          else
+            rows[i].style.display = '';
+        }
+      }
+
+      this.showPage = function (pageNumber) {
+
+        if (!this.inited) {
+          alert("not inited");
+          return;
+        }
+
+        var oldPageAnchor = document.getElementById('pg' + this.currentPage);
+       oldPageAnchor.className = 'pg-normal';
+
+        this.currentPage = pageNumber;
+        var newPageAnchor = document.getElementById('pg' + this.currentPage);
+       newPageAnchor.className = 'pg-selected';
+
+        var from = (pageNumber - 1) * (Number(itemsPerPage));
+        var to = (from + Number(itemsPerPage)) - 1;
+        this.showRecords(from, to);
+var pagerName = 'pager';
+
+        var pgNext = document.getElementById(pagerName + 'pgNext');
+
+        var pgPrev = document.getElementById(pagerName + 'pgPrev');
+
+        if (pgNext != null) {
+          if (this.currentPage == this.pages) pgNext.style.display = 'none';
+          else pgNext.style.display = '';
+        }
+        if (pgPrev != null) {
+          if (this.currentPage == 1) pgPrev.style.display = 'none';
+          else pgPrev.style.display = '';
+        }
+        var noOfPages=this.currentPage * PerPagesize;
+
+        if(noOfPages>records){
+          noOfPages = records;
+        }
+        
+        var showingPage = document.getElementById('showPage');
+        
+		//To display the cab count
+		document.getElementById("displayCount").innerText ='Display : '+ $('#cab-info > tr:not([style*="display: none"])').length+" out of " +rowCounter;
+
+
+      }
+
+      this.prev = function () {
+        if (this.currentPage > 1) {
+          this.createPager(this.currentPage - 4, this.currentPage); // it  used to move the cursor to privious index.
+          this.showPage(this.currentPage - 1);
+        }
+      }
+
+      this.next = function () {
+        if (this.currentPage < this.pages) {
+          this.createPager(this.currentPage, this.currentPage + 4);  //when i click the next button it is count the records      upto 5 . So indexing   is mainting the standed.
+
+          this.showPage(this.currentPage + 1);  //it is used to print  imediate next records.
+
+
+        }
+      }
+
+      this.init = function () {
+        var rows = driverCount;
+       records = (rows);
+        this.pages = Math.ceil(records / itemsPerPage);
+        this.inited = true;
+
+      }
+
+
+
+      //it is used to  move the cursor upto 5 records.
+      this.createPager = function (startEle, EndEle) {
+
+
+        if (EndEle <= this.pages && startEle > 0) {
+
+          var pagerName = document.querySelector(':root');
+          pagerName = 'pager';
+
+
+
+
+          var pagerHtml = ' <span id="' + pagerName + 'pgPrev" onclick="' + pagerName + '.prev();" class="pg-normal">  <img src="images/prev-arrow.jpg" alt="page-arrow" width="18" height="15"/> </span> &nbsp; ';
+
+
+          
+         var element = document.getElementById("pageNavPosition");
+          for (var page = startEle; page <= EndEle; page++)
+
+
+            pagerHtml += ' <span id="pg' + page + '" class="pg-normal" onclick="' + pagerName + '.showPage(' + page + ');">' + page + ' </span> &nbsp; ';;
+
+
+          pagerHtml += ' <span id="' + pagerName + 'pgNext" onclick="' + pagerName + '.next();" class="pg-normal">   <img id ="pagination-arrow" src="image/next-arrow.jpg" width="18" height="15" alt="page-arrow" /> </span> ';
+
+          element.innerHTML = pagerHtml;
+
+        }
+
+      }
+
+      this.showPageNav = function (pagerName, positionId) {
+
+        if (!this.inited) {
+          alert("not inited");
+          return;
+        }
+
+
+        var pagerHtml = ' <span id="' + pagerName + 'pgPrev" onclick="' + pagerName + '.prev();" class="pg-normal"> <img src="Images/pagination-prev-arrow.svg" alt="page-arrow" width="18" height="15"/> </span> &nbsp;  ';
+
+
+        //for printing number of indexes per page
+
+        if (this.pages >= 5) {
+          
+          this.totPages = 5
+        }
+        else {
+          this.totPages = this.pages;
+        }
+
+        //it is used to print the  fixed 5 records.
+        var element = document.getElementById(positionId);
+
+        for (var page = 1; page <= this.totPages; page++)
+        pagerHtml += ' <span id="pg' + page + '" class="pg-normal" onclick="' + pagerName + '.showPage(' + page + ');">' + page + '  </span> &nbsp;';
+        if(this.currentPage != this.totPages){
+
+        pagerHtml += ' <span id="' + pagerName + 'pgNext" onclick="' + pagerName + '.next();" class="pg-normal">  <img src="Images/pagination-next-arrow.svg" width="18" height="15" alt="page-arrow" />    </span> ';
+        }
+        element.innerHTML = pagerHtml;
+      }
+
+      
+    }
+
+
+
+
